309607718122053f654b58c3b9dd859e2dcc1d0a
Grainne B <grainne.bhreathnach@pearson.com>
2025-03-11 11:55:06 +1100

    working tests

--- test_model.py
+++ test_model.py
@@ -1,32 +1,49 @@
 from datetime import date, timedelta
 import pytest
 
-# from model import ...
+from model import Batch, OrderLine
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
 
 
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty)
+    )
+
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail("todo")
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 2)
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
 
 
-def test_can_allocate_if_available_greater_than_required():
-    pytest.fail("todo")
-
-
-def test_cannot_allocate_if_available_smaller_than_required():
-    pytest.fail("todo")
+def test_cannot_allocate_if_available_greater_than_required():
+    batch, line = make_batch_and_line("SMALL-TABLE", 20, 22)
+    assert not batch.can_allocate(line)
+    
+def test_can_allocate_if_available_smaller_than_required():
+    batch, line = make_batch_and_line("SMALL-TABLE", 22, 10)
+    assert batch.can_allocate(line)
 
 
 def test_can_allocate_if_available_equal_to_required():
-    pytest.fail("todo")
+    batch, line = make_batch_and_line("SMALL-TABLE", 21, 21)
+    assert batch.can_allocate(line)
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
+
+# def test_prefers_warehouse_batches_to_shipments():
+#     pytest.fail("todo")
 
 
-def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail("todo")
+# def test_prefers_earlier_batches():
+#     pytest.fail("todo")
 
 
-def test_prefers_earlier_batches():
-    pytest.fail("todo")
